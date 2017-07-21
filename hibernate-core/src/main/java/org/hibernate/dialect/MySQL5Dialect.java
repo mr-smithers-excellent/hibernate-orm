eab45bc6e0a81d1edd5a45b49fbe5d7bb4818dcb
mr-smithers-excellent <seanpsmith85@gmail.com>
2017-07-21 12:20:57 -0400

    add support for mediumtext and text column types

--- hibernate-core/src/main/java/org/hibernate/dialect/MySQL5Dialect.java
+++ hibernate-core/src/main/java/org/hibernate/dialect/MySQL5Dialect.java
@@ -22,7 +22,8 @@ public class MySQL5Dialect extends MySQLDialect {
 	@Override
 	protected void registerVarcharTypes() {
 		registerColumnType( Types.VARCHAR, "longtext" );
-//		registerColumnType( Types.VARCHAR, 16777215, "mediumtext" );
+		registerColumnType( Types.VARCHAR, 16777215, "mediumtext" );
+		registerColumnType( Types.VARCHAR, 65535, "text" );
 		registerColumnType( Types.VARCHAR, 65535, "varchar($l)" );
 		registerColumnType( Types.LONGVARCHAR, "longtext" );
 	}
